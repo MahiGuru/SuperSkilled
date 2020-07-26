86f32b69daf2bbfb1a853fa125fca268a86476e5
Mahipal Gurjala <Mahipal.Gurjala@planonsoftware.com>
2020-07-27 00:42:01 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const RequestQuote = lazy(() => import('./pages/Request-Quote'));
+const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/quote">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/quote">
+            <RequestQuote />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Suspense>
+  </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home: React.FC = () => (
+  <h1>This is a Request Quote page</h1>
+);
+
+export default Home;
